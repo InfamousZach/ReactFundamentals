dc83fb855702eb83b16a1ccc7c2a95cb2853c421
Zachary Amstutz <zachamstutz1@gmail.com>
2024-04-16 21:19:11 -0400

    Added header, sidebar, and footer to respective files, along with some styling for each component.

---
+++ src/Componets/Site/Footer.js
@@ -0,0 +1,11 @@
+import {Row} from 'reactstrap';
+    
+    const Footer = () => {
+        return(
+            <footer>
+                <Row>
+                    <p>© Eleven Fifty 2021</p>
+                </Row>
+            </footer>
+        );
+    };
