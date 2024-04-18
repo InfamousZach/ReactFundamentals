1a59a1f939e8524554d1989353a7d14801f326e2
Zachary Amstutz <zachamstutz1@gmail.com>
2024-04-18 18:11:18 -0400

    Added in the single element rule

--- src/Components/Site/Sidebar.js
+++ src/Components/Site/Sidebar.js
@@ -1,30 +1,32 @@
-import {
-            Route,
-            Link,
-            Routes
-        } from 'react-router-dom';
-        import Home from './Home';
-        import Resources from './Resources';
+    import {
+        Route,
+        Link,
+        Routes
+    } from 'react-router-dom';
+    import Home from './Home';
+    import Resources from './Resources';
+    import FunctionalComponentDemo from '../concepts/FunctionalComponentDemo';
         
-        const Sidebar = () => {
-           return(
-               <div className='sidebar'>
-                   <div className='sidebar-list-styling'>
-                       <ul className='sidebar-list list-unstyled'>
-                           <li><Link to='/'>Home</Link></li>
-                           <li><Link to='/resources'>Resources</Link></li>
-                           <li><Link to='/functionalcomponent'>Functional Component</Link></li>
-                       </ul>
-                   </div>
-                   <div className='sidebar-route'>
-                       <Routes>
-                           <Route exact path='/home' element={<Home />}/>
-                           <Route exact path='/resources' element={<Resources />}/>
-                           <Route exact path='/' element={<Home />}/>
-                       </Routes>
-                   </div>
-               </div>
-           );
-       };
+    const Sidebar = () => {
+        return(
+            <div className='sidebar'>
+                <div className='sidebar-list-styling'>
+                    <ul className='sidebar-list list-unstyled'>
+                        <li><Link to='/'>Home</Link></li>
+                        <li><Link to='/resources'>Resources</Link></li>
+                        <li><Link to='/functionalcomponent'>Functional Component</Link></li>
+                    </ul>
+                </div>
+                <div className='sidebar-route'>
+                    <Routes>
+                        <Route exact path='/home' element={<Home />}/>
+                        <Route exact path='/resources' element={<Resources />}/>
+                        <Route exact path='/' element={<Home />}/>
+                        <Route exact path='/functionalcomponent'element={<FunctionalComponentDemo />}/>
+                    </Routes>
+                </div>
+            </div>
+        );
+    };
        
-       export default Sidebar;
+    export default Sidebar;
---
+++ src/Components/concepts/FunctionalComponents/FunctionalComponentDemo.js
@@ -0,0 +1,18 @@
+import '../../../App.css';
+
+const FunctionalComponentDemo = () => {
+    return(
+        <div className='main'>
+            <div className='mainDiv'>
+                <div>
+                Hello React
+                </div>
+                <div>
+                How are you today?
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default FunctionalComponentDemo;
